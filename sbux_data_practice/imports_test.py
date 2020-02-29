ff3d1d555b4f2c379fb6289781d5c164ff39c149
amadhav2021 <amadhav2021@gmail.com>
2020-02-29 10:12:48 -0500

    read in csv and created point map

---
+++ sbux_data_practice/imports_test.py
@@ -0,0 +1,7 @@
+import folium
+import pandas as pd
+import json
+
+print("Panda version: " + pd.__version__)
+print("Folium version: " + folium.__version__)
+print("JSON version: " + json.__version__)
