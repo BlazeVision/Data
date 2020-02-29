387a3c757ea00bdbce7f14c0218dbd16615b6d66
amadhav2021 <amadhav2021@gmail.com>
2020-02-29 11:56:49 -0500

    read in csv and created point map html file

--- sbux_data_practice/point_map.py
+++ sbux_data_practice/point_map.py
@@ -2,13 +2,21 @@ import folium
 import pandas as pd
 import json
 
+# read in the coordinates data file
 df = pd.read_csv('starbucksInLACounty.csv')
 
+# read in the LA map json file to highlight LA county
 with open('laMap.geojson') as f:
     laArea = json.load(f)
 
 # initialize the map around LA County
-laMap = folium.Map(location=[34.0522, -118.2437], tiles='Stamen Toner', zoom_start=9)
+# location centers the map to LA (default is world map)
+# tiles changes the display
+# zoom_start is an int that zooms in the map (bigger int means more zoomed in)
+
+# laMap = folium.Map(location=[34.0522, -118.2437], tiles='Stamen Toner', zoom_start=9)
+laMap = folium.Map(location=[34.0522, -118.2437], tiles='OpenStreetMap', zoom_start=9)
+
 
 # add the shape of LA County to the map
 folium.GeoJson(laArea).add_to(laMap)
